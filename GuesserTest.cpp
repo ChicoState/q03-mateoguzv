123ec995eb664c22109bb26c210cd6ecf02def97
mateoguzv <mguzmanvanegas@mail.csuchico.edu>
2025-04-30 15:13:58 -0700

    Quiz 03: Unit Testing final sol

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,64 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+
+
+TEST(GuesserTest, WrongGuessWithSmallDistanceDecreasesRemaining){
+    Guesser g("hello");
+    ASSERT_FALSE(g.match("hella")); 
+    ASSERT_EQ(2,g.remaining());
 }
+
+TEST(GuesserTest, WrongGuessWithBigDistanceLocksOut)
+{
+    Guesser g("hello");
+    ASSERT_FALSE(g.match("xxxxx")); 
+    ASSERT_FALSE(g.match("hello")); 
+    ASSERT_EQ(3, g.remaining()); 
+
+}
+
+TEST(GuesserTest,LockedOutAfterThreeWrongSmallDistanceGuesses){
+    Guesser g("code");
+    g.match("cade"); 
+     g.match("cade");  
+    g.match("cade"); 
+ 
+    ASSERT_FALSE(g.match("code"));
+      ASSERT_EQ(0, g.remaining());
+
+}
+
+TEST(GuesserTest,DistanceExactMatchIsZero){
+    Guesser g("test" );
+    ASSERT_EQ(0,g.distance("test"));
+}
+
+TEST(GuesserTest, DistanceOneWrongCharacter){
+    	Guesser g("test");
+    ASSERT_EQ(1, g.distance("tent"));
+}
+
+TEST(GuesserTest,DistanceLongGuessCapped){
+    Guesser g("short");
+    ASSERT_EQ(5, g.distance("averyverylongstringthatexceeds")); 
+}
+
+TEST(GuesserTest, DistanceShortGuessAddsMissing){
+    Guesser g("secret");
+     ASSERT_EQ(3,g.distance("sec"));
+}
+
+TEST(GuesserTest, SecretTruncatedTo32Chars){
+    std::string long_secret(100, 'x');
+    Guesser g(long_secret);
+     ASSERT_EQ(0,g.distance(std::string(32, 'x'))); 
+}
+
+TEST(GuesserTest, MatchFailsIfLocked){
+    Guesser g("secure");
+    g.match("wrongwrongwrong"); 
+    ASSERT_FALSE(g.match("secure")); 
+}
+
+
